fb95056c7e3f476c80816b2304c5adad9aa70886
David Alsh <alshdavid@gmail.com>
2021-04-15 06:05:03 -0700

    update

--- packages/sandbox/src/index.tsx
+++ packages/sandbox/src/index.tsx
@@ -7,7 +7,8 @@ void async function() {
   const { port1, port2 } = new MessageChannel()
   const worker = new Worker()
   worker.postMessage(undefined, [port2])
-
+  port1.start()
+  
   const h0 = createHandle<Source>(port1)
   console.log(await h0.property('foo').value())
 }()
--- packages/sandbox/src/index.worker.ts
+++ packages/sandbox/src/index.worker.ts
@@ -3,7 +3,6 @@ import { Source } from './types'
 
 self.addEventListener('message', event => {
   const [ port2 ] = event.ports
-
   const source: Source = { foo: 'bar' }
   createSource(port2, source)
   port2.start()
