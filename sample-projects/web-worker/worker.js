fdeb6a02d21c45ab466e657d705bd238fa6f26cc
David Alsh <alshdavid@gmail.com>
2021-11-02 23:28:27 +1100

    starter projects

--- sample-projects/web-worker/worker.js
+++ sample-projects/web-worker/worker.js
@@ -1,14 +1,16 @@
 import 'https://cdn.davidalsh.com/rpc/latest.js'
+// import 'http://localhost:8080/dist/umd/index.js'
 
 // Create and transfer ports
 const { port1, port2 } = new MessageChannel()
-self.postMessage('PORT_TRANSFER', '*', [port2])
+self.postMessage('PORT_TRANSFER', [port2])
 port1.start()
 
 // Access exposed Window from host page
 const ref0 = new RPC.Reference(port1)
 
-// Make a div
+// // Make a div
 const ref1 = await ref0.property('document', 'createElement').exec('div')
 await ref1.property('innerText').set('Hello')
 const ref2 = await ref0.property('document', 'body', 'appendChild').exec(ref1)
+
